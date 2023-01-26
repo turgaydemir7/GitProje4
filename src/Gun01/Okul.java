cd32cfc5c521b93b1885b945687947dff0c7c464
trgyd <trgydmr17@gmail.com>
2023-01-26 19:38:09 +0300

    task 4 yapıldı

--- src/Gun01/Okul.java
+++ src/Gun01/Okul.java
@@ -3,7 +3,7 @@ package Gun01;
 public class Okul {
     public static void main(String[] args) {
 
-        System.out.println("okuldan merhaba ");
+        System.out.println("okuldan merhaba");
     }
 
 }
